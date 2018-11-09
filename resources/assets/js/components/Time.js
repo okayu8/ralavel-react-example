7955737aacd6f232a667ced1ea58584a968e48da
片岡祐太郎 <hitugaya12@gmail.com>
2018-11-10 00:10:40 +0900

    20181109

--- resources/assets/js/components/Time.js
+++ resources/assets/js/components/Time.js
@@ -62,26 +62,5 @@ class Time extends Component {
     }
 }
 
-/* class Time extends Component {
 
-
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 col-md-offset-2">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">Example Component</div>
-
-                            <div className="panel-body">
-                                This is Time Counter!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-} */
 export default Time;
