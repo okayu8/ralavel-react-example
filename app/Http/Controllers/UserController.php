64fa1eb9dc4a046b43db35aa5d58fd2268f89f44
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-01-06 21:29:46 +0900

    add userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 use App\User;
 use JWTAuth;
 use JWTAuthException;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    
+    public function index(Request $request) {
+        $user = Auth::user();   #ログインユーザー情報を取得します。
+        return response()->json($user);
+    }
 }
 
