9936937a8399cb7082ab4b2f3c08fade5cb0193a
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-03-05 00:27:36 +0900

    add getTodoAction

--- resources/assets/js/apiController/ActionApi.js
+++ resources/assets/js/apiController/ActionApi.js
@@ -3,11 +3,14 @@ export const getTodoAction = () => {
         .then(response => {
             console.log('todos_list')
             console.log('data:' + JSON.stringify(response.data))
-            this.setState({
-                data: response.data.data,
-                nextUrl: response.data.next_page_url,
-                prevUrl: response.data.prev_page_url,
-            })
+            const todos = [
+                {
+                    "data": response.data.data,
+                    "nextURL": response.data.next_page_url,
+                    "prevUrl": response.data.prev_page_url
+                }
+            ]
+            return todos;
         })
         .catch(function (error) {
             console.log(error)
