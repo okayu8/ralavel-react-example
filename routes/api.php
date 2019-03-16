e535de46d8e77c67e27f81b02cc538a16a49a280
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-03-17 01:39:31 +0900

    fixing api.php

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
-//Route::get('logout', 'yourSessionController@logout');
+// Route::resource('todos', 'ToDosController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+
+// Route::resource('done', 'DoneController', ['only' => ['index', 'show', 'update', 'destroy']]);
+
+// Route::resource('date', 'DateController', ['only' => ['index']]);
+
+// Route::resource('isLogin', 'CheckController', ['only' => ['index']]);
