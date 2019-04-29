d19ea0ecd21561a76f01e9dccf02600f3aae93c6
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-29 14:13:05 +0900

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Quick Todo</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        {{-- <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"> --}}
     </head>
     <body>
         @if (Session::has('flash_error_message'))
