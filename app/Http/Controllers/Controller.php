810a3db143d9ebc7340e1ddbdbd6172efcf91f74
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-05-11 11:54:15 +0900

    add laravel debugger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+        public function __construct()
+    {
+
+        //キャッシュが残るので 毎回 view ファイルを削除する
+        $success = \File::cleanDirectory(storage_path()."/framework/views/");
+
+        if(\Request::ip() == "61.27.15.240"){
+
+            //デバッグモード
+
+        } else {
+            \Debugbar::disable();
+        }
+
+
+    }
 }
